9c75fb73e080871cde936a775855bca3374556c6
Jonathan Vo <fowlchi@gmail.com>
2022-03-14 22:00:29 -0700

    hw4 finalization

--- R/MCMC_beta.R
+++ R/MCMC_beta.R
@@ -8,13 +8,15 @@ MCMC_beta <- function(x0,N,alph=4,bet=2,sigma=0.125) {
   mean_trace = rep(0,N);
   MCMC_trace = rep(0,N);
   stdmean_trace = rep(0,N);
+  std_trace = rep(0,N);
   MCMC_trace[1] = x0;
-  mean_trace[1] = x0;
+  MCMC_sq_trace[1] = x0^2;
+  MCMC_sq_mean_trace[1] = x0^2;
   stdmean_trace[1] = 0;
   for (n in 2:N) {
 
     x1_temp = rlnorm(1,meanlog=log(MCMC_trace[n-1]),sdlog=sigma);
-    hastings = dlnorm(MCMC_trace[n-1],meanlog=log(x1_temp),sdlog=sga)*dbeta(x1_temp,alph,bet)/(dlnorm(x1_temp,meanlog=log(MCMC_trace[n-1]),sdlog=sga)*dbeta(MCMC_trace[n-1],alph,bet));
+    hastings = dlnorm(MCMC_trace[n-1],meanlog=log(x1_temp),sdlog=sigma)*dbeta(x1_temp,alph,bet)/(dlnorm(x1_temp,meanlog=log(MCMC_trace[n-1]),sdlog=sigma)*dbeta(MCMC_trace[n-1],alph,bet));
     #hastings = ((x1_temp^4) * (1-x1_temp))/((MCMC_trace[n-1]^4)*(1-MCMC_trace[n-1]));
     acc = min(hastings,1);
     u = runif(1);
@@ -25,8 +27,6 @@ MCMC_beta <- function(x0,N,alph=4,bet=2,sigma=0.125) {
       x1 = MCMC_trace[n-1];
     }
     MCMC_trace[n] = x1;
-    mean_trace[n] = mean(MCMC_trace[1:n])
-    stdmean_trace[n] = sd(mean_trace[1:n]);
   }
-  return(list(MCMC_trace,mean_trace,stdmean_trace))
+  return(MCMC_trace)
 }
